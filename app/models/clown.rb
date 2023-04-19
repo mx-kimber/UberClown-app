1ebe1b2fc881865943ba119a1d3cca90fdde46c0
Kimber Ogden <mx.kimber@gmail.com>
2023-04-19 15:00:00 -0500

    Add controllers and jbuilders

--- app/models/clown.rb
+++ app/models/clown.rb
@@ -1,2 +1,4 @@
 class Clown < ApplicationRecord
+  has_many :bookings
+  has_many :customers, through: :bookings
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+  has_many :bookings
+  has_many :clowns, through: :bookings
 end
